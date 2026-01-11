18e1107526cb9220c7e67bf80420b206ab40bb09
superintendent2521 <67196752+superintendent2521@users.noreply.github.com>
2026-01-10 22:07:42 -0500

    dear fucking god

--- parser.py
+++ parser.py
@@ -1,2 +1,14 @@
 # im going to go fucking insane
-#
+
+
+s = input()
+parts = s.split(" ") # Space here splits it so i can do logic
+
+
+print(parts)
+num1 = int(parts[0]) # fucked up example of getting both ints?
+num2 = int(parts[2])
+
+if 'PLUS' in parts:
+    output = num1 + num2
+    print(output)
